64602be24bab6247dfbb50107e49f04cd3ee0d56
anupam-singhdeo_ModMed <anupam.singhdeo@modmed.com>
2026-03-18 17:44:17 +0530

    Restore all source files

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-
-export default defineConfig({
-  base: '/apmo-pi-planner-app/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/apmo-pi-planner-app/' : '/',
   plugins: [
     react({
       babel: {
@@ -12,4 +11,4 @@ export default defineConfig({
       },
     }),
   ],
-})
+}))
